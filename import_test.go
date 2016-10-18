f59ed20f2b7024942b9a18ec48cc7eaacb6a27b2
Matthew Cunningham <matt@mattcunningham.net>
2016-10-18 10:36:37 -0500

    EVERYTHING WORKS

--- import_test.go
+++ import_test.go
@@ -3,15 +3,27 @@ package main
 import "testing"
 
 func TestCSVtoEntries(t *testing.T) {
-	in := `Grade, Sibling, Faculty, Fayetteville
-8, No, No, Yes
-9, Yes, No, No`
-	entries := CSVToEntries(in)
+	in := `Grade, Sibling, Faculty, First name
+8,,,Matthew
+9,YES,,Davis
+10,,YES,Jade`
+	entries := CSVtoEntries(in)
 	expectedOutput := []Entry{
-		Entry{Status: 0, Priority: 0, Info: map[string]string{"Fayetteville": "Yes"}},
-		Entry{Status: 0, Priority: 1, Info: map[string]string{"Fayetteville": "No"}},
+		Entry{Status: 0, Grade: 8, Priority: 0, Info: map[string]string{"First name": "Matthew"}},
+		Entry{Status: 0, Grade: 9, Priority: 1, Info: map[string]string{"First name": "Davis"}},
+		Entry{Status: 0, Grade: 10, Priority: 2, Info: map[string]string{"First name": "Jade"}},
 	}
-	if entries != expectedOutput {
-		t.Error("CSVToEntries did not return the expected output.")
+	for i, entry := range entries {
+		expectedOutputEntry := expectedOutput[i]
+		if entry.Status != expectedOutputEntry.Status {
+			t.Error("Expected status does not match actual status.")
+		}
+		if entry.Grade != expectedOutputEntry.Grade {
+			t.Error("Expected grade does not match actual grade.")
+		}
+		if entry.Priority != expectedOutputEntry.Priority {
+			t.Error("Expected priority does not match actual priority.")
+		}
+
 	}
-)
+}
