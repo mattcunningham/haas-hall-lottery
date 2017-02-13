48f366202273997fb2869a461e0ec1861d5460ea
Matthew Cunningham <matt@mattcunningham.net>
2017-02-13 09:20:27 -0600

    Removing unnecessary whitespace in front of javascript code

--- main.go
+++ main.go
@@ -37,114 +37,114 @@ const (
   integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
   crossorigin="anonymous"></script>
     <script type="text/javascript">
-		$(":file").change(function() {
-		  var file = this.files[0], result;
-		  if (file) {
-		    var reader = new FileReader;
-		    reader.readAsText(file);
-		    reader.onload = function(e) {
-		      result = e.target.result;
-		      $.ajax({ url: "post", type: "POST", dataType: "json", success: completeHandler,
-		        data: {
-		          data: result,
-		          entries: prompt("How many people do you want to admit?")
-		        }
-		      });
-		    };
-		  }
-		});
-		function completeHandler(serverData, textStatus) {
-		  outputFile = "";
-		  var header = createHeader(serverData[0]);
-		  var headerOutput = header[0], headerFile = header[1];
-		  $("#fromServer").append(headerOutput);
-		  outputFile += headerFile;
-		  serverData.forEach(function(entry) {
-		    var ret = createElement(entry);
-		    var forOutput = ret[0];
-		    var forFile = ret[1];
-		    $("#fromServer").append(forOutput);
-		    $("#export").css("display", "block");
-		    outputFile += forFile;
-		  });
-		  $("#getDataButton").css("display", "none");
-		  var a = document.getElementById("export");
-		  var file = new Blob([outputFile], { type: "text/plain" });
-		  a.href = URL.createObjectURL(file);
-		  a.download = "results.csv";
-		}
-		function createHeader(entry) {
-		  fileData = ""
-		  output = '<div class="entry header">'
-		  for (var key in entry) {
-		    if (key === "Grade" || key === "Priority") {
-		      fileData += key + ",";
-		    } else if (entry[key] === Object(entry[key])) {
-		      for (var objKey in entry[key]) {
-		        fileData += objKey + ",";
-		      }
-		    } else {
-		      fileData += key + ",", output += '<span class="entry-item header">' + key + "</span>";
-		    }
-		  }
-		  fileData = fileData.substring(0, fileData.length - 1) + "\n";
-		  return [output, fileData];
-		}
-		function createElement(entry) {
-		  index = 0;
-		  output = '<div class="entry">';
-		  fileData = "";
-		  for (var key in entry) {
-		    if (key === "Grade" || key === "Priority") {
-		      fileData += formatCSV(key, entry[key]) + ",";
-		    } else if (entry[key] === Object(entry[key])) {
-		      for (var objKey in entry[key]) {
-		        fileData += entry[key][objKey] + ",";
-		      }
-		    } else { // this is the real information
-		      fileData += formatCSV(key, entry[key]) + ",";
-		      output += '<span class="entry-item ' + ((index % 2 == 0) ? "even" : "odd") + '">' + formatAdmittance(key, entry[key]) + "</span>";
-		      index++;
-		    }
-		  }
-		  fileData = fileData.substring(0, fileData.length - 1) + "\n";
-		  output += "</div>\n";
-		  return [output, fileData];
-		}
-		var waitListCounter = 0;
-		function formatCSV(key, info) {
-		  if (key !== "Status" && key !== "Priority") {
-		    return info
-		  }
-		  if (key === "Status") {
-		    if (info == 2) {
-		      return "Admitted";
-		    } else if (info == 1) {
-		      waitListCounter++;
-		      return "Wait list " + waitListCounter;
-		    }
-		  }
-		  if (key === "Priority") {
-		    return info == 2 ? "Faculty" : (info == 1 ?  "Sibling" : "");
-		  }
-		}
-		var waitListAdmitCounter = 0;
-		function formatAdmittance(key, info) {
-		  if (key !== "Status" && key !== "Priority") {
-		    return info;
-		  }
-		  if (key === "Status") {
-		    if (info == 2) {
-		      return "<span id=\"admitted\">Admitted</span>";
-		    } else if (info == 1) {
-		      waitListAdmitCounter++;
-		      return '<span id="waitlisted">Wait list <span id="waitlist-count">' + waitListAdmitCounter + "</span></span>";
-		    }
-		  }
-		  if (key === "Priority") {
-		    return 2 == info ? '<span id="faculty">Faculty</span>' : (1 == info ? '<span id="sibling">Sibling</span>' : "");
-		  }
-		}
+$(":file").change(function() {
+  var file = this.files[0], result;
+  if (file) {
+    var reader = new FileReader;
+    reader.readAsText(file);
+    reader.onload = function(e) {
+      result = e.target.result;
+      $.ajax({ url: "post", type: "POST", dataType: "json", success: completeHandler,
+        data: {
+          data: result,
+          entries: prompt("How many people do you want to admit?")
+        }
+      });
+    };
+  }
+});
+function completeHandler(serverData, textStatus) {
+  outputFile = "";
+  var header = createHeader(serverData[0]);
+  var headerOutput = header[0], headerFile = header[1];
+  $("#fromServer").append(headerOutput);
+  outputFile += headerFile;
+  serverData.forEach(function(entry) {
+    var ret = createElement(entry);
+    var forOutput = ret[0];
+    var forFile = ret[1];
+    $("#fromServer").append(forOutput);
+    $("#export").css("display", "block");
+    outputFile += forFile;
+  });
+  $("#getDataButton").css("display", "none");
+  var a = document.getElementById("export");
+  var file = new Blob([outputFile], { type: "text/plain" });
+  a.href = URL.createObjectURL(file);
+  a.download = "results.csv";
+}
+function createHeader(entry) {
+  fileData = ""
+  output = '<div class="entry header">'
+  for (var key in entry) {
+    if (key === "Grade" || key === "Priority") {
+      fileData += key + ",";
+    } else if (entry[key] === Object(entry[key])) {
+      for (var objKey in entry[key]) {
+        fileData += objKey + ",";
+      }
+    } else {
+      fileData += key + ",", output += '<span class="entry-item header">' + key + "</span>";
+    }
+  }
+  fileData = fileData.substring(0, fileData.length - 1) + "\n";
+  return [output, fileData];
+}
+function createElement(entry) {
+  index = 0;
+  output = '<div class="entry">';
+  fileData = "";
+  for (var key in entry) {
+    if (key === "Grade" || key === "Priority") {
+      fileData += formatCSV(key, entry[key]) + ",";
+    } else if (entry[key] === Object(entry[key])) {
+      for (var objKey in entry[key]) {
+        fileData += entry[key][objKey] + ",";
+      }
+    } else { // this is the real information
+      fileData += formatCSV(key, entry[key]) + ",";
+      output += '<span class="entry-item ' + ((index % 2 == 0) ? "even" : "odd") + '">' + formatAdmittance(key, entry[key]) + "</span>";
+      index++;
+    }
+  }
+  fileData = fileData.substring(0, fileData.length - 1) + "\n";
+  output += "</div>\n";
+  return [output, fileData];
+}
+var waitListCounter = 0;
+function formatCSV(key, info) {
+  if (key !== "Status" && key !== "Priority") {
+    return info
+  }
+  if (key === "Status") {
+    if (info == 2) {
+      return "Admitted";
+    } else if (info == 1) {
+      waitListCounter++;
+      return "Wait list " + waitListCounter;
+    }
+  }
+  if (key === "Priority") {
+    return info == 2 ? "Faculty" : (info == 1 ?  "Sibling" : "");
+  }
+}
+var waitListAdmitCounter = 0;
+function formatAdmittance(key, info) {
+  if (key !== "Status" && key !== "Priority") {
+    return info;
+  }
+  if (key === "Status") {
+    if (info == 2) {
+      return "<span id=\"admitted\">Admitted</span>";
+    } else if (info == 1) {
+      waitListAdmitCounter++;
+      return '<span id="waitlisted">Wait list <span id="waitlist-count">' + waitListAdmitCounter + "</span></span>";
+    }
+  }
+  if (key === "Priority") {
+    return 2 == info ? '<span id="faculty">Faculty</span>' : (1 == info ? '<span id="sibling">Sibling</span>' : "");
+  }
+}
     </script>
   </body>
 </html>`
